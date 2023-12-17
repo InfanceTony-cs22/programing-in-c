66148d433a6367b0e9e9225ab50225d90a5f8b18
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-17 12:11:33 +0530

    Update Day-8.c

--- Day-8.c#include <stdio.h> #include <string.h> #include <math.h> #include <stdlib.h> /Day-8.c
+++ Day-8.c#include <stdio.h> #include <string.h> #include <math.h> #include <stdlib.h> /Day-8.c
@@ -40,3 +40,44 @@ scanf("%d",&k);
 
     return 0;
 }
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+    int max_and=0,max_or=0,max_xor=0;
+
+    for (int i=1;i<=n;i++) {
+        for (int j=i+1;j<=n;j++) {
+            int and_result=i&j;
+            int or_result=i|j;
+            int xor_result=i^j;
+
+            if (and_result<k && and_result>max_and) {
+                max_and=and_result;
+            }
+            if (or_result<k && or_result>max_or) {
+                max_or=or_result;
+            }
+            if (xor_result<k && xor_result>max_xor) {
+                max_xor=xor_result;
+            }
+        }
+    }
+
+    printf("%d",max_and);
+     printf("\n%d",max_or);
+     printf("\n%d",max_xor);
+}
+
+int main()
+ {
+    int n, k;
+scanf("%d",&n);
+scanf("%d",&k);
+    calculate_the_maximum(n, k);
+
+    return 0;
+}
